1de7a8e7c78e0d1c5aa6d517f62bee25b2f7b0ef
cecilech <cecile.chomienne@edu.ece.fr>
2017-03-08 00:34:30 +0100

    Create function getAffMatrix() in class Admin

--- src/firstTry/Admin.java
+++ src/firstTry/Admin.java
@@ -24,6 +24,52 @@ public class Admin {
 	}
 	
 	
+	public double[][] getAffMatrix(){
+	/* this fct generates a matrix giving a percentage for each hour of each day for caf paris 15*/
+	/* to understand my idea:
+	 *   from figures gathered on the CAF website we can find an average of people coming per day 
+	 *   -->around 2400 for caf paris 15
+	 *   with the percentages apply to that nb for each hour we can get random nb of people coming per hour per day
+	 *   we don't care if the sum for each day is > 100% bc the nb of people coming has to fluctuate 
+	 */
+		
+	
+		
+		final Random random = new Random();
+				
+		double weak=0.08; //weak affluence, perc. of total nb of people coming per day in pondered average
+		double moderate=0.15; //moderate aff
+		double strong=0.30; //strong aff
+		double delta=0.1; 
+		double w=0.0;
+		double m=0.0;
+		double s=0.0;
+		
+		double[][] aff =
+		    {
+		        { m, m, s, s, m, m, s, s } , // tab[day][hour] 
+		        { w, w, m, m, m, m, w, w },    //as we take caf paris 15 as example, only four opening days
+		        { w, w, w, w, w, w, m, m },   //monday tuesday thursday friday, from 9am to 5pm 
+		        { m, m, m, m, m, m, s, s },
+		    };
+		
+		/*BUG dans les if, passe que le premier*/
+		for (int i=0;i<aff.length;i++){
+			for (int j=0;j<aff[i].length;j++){
+				if (aff[i][j]==w)
+					aff[i][j]=(weak-weak*delta) + ((weak+weak*delta) - (weak-weak*delta)) * random.nextDouble();
+				if (aff[i][j]==m)
+					aff[i][j]=(moderate-moderate*delta) + ((moderate+moderate*delta) - (moderate-moderate*delta)) * random.nextDouble();
+				if (aff[i][j]==s)
+					aff[i][j]=(strong-strong*delta) + ((strong+strong*delta) - (strong-strong*delta)) * random.nextDouble();
+				System.out.print(aff[i][j]+" ");
+			}
+			System.out.println();
+		}
+		return aff;
+	}	   
+	
+	
 	
     public static void main(String[] args) {
     	
@@ -38,5 +84,7 @@ public class Admin {
 		System.out.println(time.toString());*/
 		
 		
+    	admin.getAffMatrix();
+   	
 	}
 }
