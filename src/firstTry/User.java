b0caf5d83290d76ee5224e75e0b211ccc6d0acaf
cecilech <cecile.chomienne@edu.ece.fr>
2017-03-07 13:14:30 +0100

    First Commit

---
+++ src/firstTry/User.java
@@ -0,0 +1,25 @@
+package firstTry;
+
+public class User {
+
+	private int id;
+	private int randNb;
+		
+	
+	User (int id){
+		this.id=id;
+	}
+	
+	public void generatePop(int randNb){
+		for (int i=0;i<randNb;i++){
+			
+			
+			
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
