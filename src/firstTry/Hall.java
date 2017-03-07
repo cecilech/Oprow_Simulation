ad9532bce20cf0757d3f5258fb9c60ea319df39e
elode0 <dolejean03@gmail.com>
2017-03-07 19:21:15 +0100

    Improving Hall Improving Borne

--- src/firstTry/Hall.java
+++ src/firstTry/Hall.java
@@ -1,5 +1,6 @@
 package firstTry;
 
+import java.sql.Time;
 import java.util.List;
 
 /**
@@ -8,6 +9,45 @@ import java.util.List;
 public class Hall {
     List<User> borneQRQueue; // list of user waiting  for a QR borne
     List<User> borneNoQRQueue; // list of user waiting for a non QR borne
+    List<Borne> bornesQR;
+    List<Borne> bornesNoQR;
 
+    Hall(List<User> aBorneQRQueue, List<User> aBorneNoQRQueue, List<Borne> aBornesQR, List<Borne> aBornesNoQR){
+        borneNoQRQueue = aBorneNoQRQueue;
+        borneQRQueue = aBorneQRQueue;
+        bornesQR = aBornesQR;
+        bornesNoQR = aBornesNoQR;
+    }
 
+    ////////////////////////////////////////////////////////////////////
+    //divideUserToBorn
+    // Divide User to a borne Queue
+    /////////////////////////////////////////////////////////////////////
+    public void divideUserToBorn(User user){
+        if(user.getPriority() == "1" || user.getPriority() == "2" || user.getPriority() == "3"){
+            borneQRQueue.add(user);
+        }
+        else if(user.getPriority() == "4" || user.getPriority() == "5" || user.getPriority() == "6"){
+            borneNoQRQueue.add(user);
+        }
+        else{
+            System.out.println("ERROR, USER " + user.getId() + "NON HAVE CORRECT STATUS");
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////
+    //assignUserToBorne
+    //assign an User to a Borne from queue
+    /////////////////////////////////////////////////////////////////////
+    public void assignUserToBorne(List<User> listUser, List<Borne> listBornes){
+        for(User user : listUser ) {
+            for (Borne borne : listBornes) {
+                if (borne.getTimeNeed() != null ) {
+                    borne.freeBorne();
+                    borne.inProgress = user;
+                    //borne.setTimeNeed(); // /!\ à compléter avec classe temps des Besoins
+                }
+            }
+        }
+    }
 }
