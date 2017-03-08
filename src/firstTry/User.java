37e366c8cd38f9bc25f604b5c46a0bf9b17799a1
cecilech <cecile.chomienne@edu.ece.fr>
2017-03-08 16:54:52 +0100

    Add auto increment method for id in class User

--- src/firstTry/User.java
+++ src/firstTry/User.java
@@ -2,11 +2,14 @@ package firstTry;
 
 import java.sql.Time;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class User {
 
-	private String id;
+	private int id;
+	private static AtomicInteger next_id = new AtomicInteger(0);
+	
 	private String need; 
 	//private float clientPerf; 
 	private Time arrivalTime = new Time(0,0,0); // when user arrives at admin
@@ -16,8 +19,9 @@ public class User {
 	private String priority; // 1:app+rdv ; 2:app+no_rdv ; 3:app+no_doc ; 4:no_app+loggin ; 5:no_app+first_Login ; 6:no_app+no_doc
 	private String numGuichet;
 	
-	User (String id, String need, long a, long b,long c,long d,String priority, String numGuichet){
-		this.id=id;
+	User (String need, long a, long b,long c,long d,String priority, String numGuichet){
+		
+		this.id=User.next_id.incrementAndGet();
 		this.need="";
 		arrivalTime.setTime(a);
 		startQueueTime.setTime(b);
@@ -42,14 +46,24 @@ public class User {
 		return user;
 	}
 	
-	public Time getRandArrivingTime(Time beginning, Time end){
+	/*public Time getRandArrivingTime(Time beginning, Time end){
 		final Random random = new Random();
 		long timeInMs=(long)random.nextInt(((int)end.getTime()-(int)beginning.getTime()+1))+(int)beginning.getTime();
 		Time arrivingTime = new Time(timeInMs);
 		System.out.println("Arriving Time: "+arrivingTime);
 		arrivalTime=arrivingTime;
 	    return arrivalTime;
+	}*/
+	
+	public Time getRandArrivingTime(long beginning, long end){
+		final Random random = new Random();
+		long timeInMs=(long)random.nextInt(((int)end-(int)beginning+1))+(int)beginning;
+		Time arrivingTime = new Time(timeInMs);
+		//System.out.println("Arriving Time: "+arrivingTime);
+		arrivalTime=arrivingTime;
+	    return arrivalTime;
 	}
+	
 	
 	public String getRandService(){
 		final Random random = new Random();
@@ -60,22 +74,15 @@ public class User {
 		return need;
 	}
 
-
-	public static void main(String[] args) {
-		User moi=new User("1234",null,0,0,0,0,null,null);
-		moi.getRandService();
-		moi.toString();
-	}
-
 	public void setStartQueueTime(Time startQueueTime) {
 		this.startQueueTime = startQueueTime;
 	}
 
-	public String getId() {
+	public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -102,6 +109,12 @@ public class User {
     public void setPriority(String priority) {
         this.priority = priority;
     }
+    
+	public static void main(String[] args) {
+		User moi=new User(null,0,0,0,0,null,null);
+		moi.getRandService();
+		moi.toString();
+	}
 }
 
 	
